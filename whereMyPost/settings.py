eff8d7fc611411a2d4d15c7fb71a63450d5023a3
lostcatbox <lostcatbox@gmail.com>
2020-12-21 00:12:39 +0900

    gg

--- whereMyPost/settings.py
+++ whereMyPost/settings.py
@@ -21,11 +21,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '=z#xqdq$0d%alf#w1-3#g@ikw0hm!x^*&yzy4$j#o0+t9$vhp&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1',"*"]
+S
 
 # Application definition
 
