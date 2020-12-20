5b9d244e8bcc7eb68bcabf66d9fcb00ae59d9bde
lostcatbox <lostcatbox@gmail.com>
2020-12-21 00:09:24 +0900

    gg

--- whereMyPost/settings.py
+++ whereMyPost/settings.py
@@ -25,23 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-secret_file = os.path.join(BASE_DIR, 'secrets.json') # secrets.json 파일 위치를 명시
-
-with open(secret_file) as f:
-    secrets = json.loads(f.read())
-
-def get_secret(setting, secrets=secrets):
-    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
-    try:
-        return secrets[setting]
-    except KeyError:
-        error_msg = "Set the {} environment variable".format(setting)
-        raise ImproperlyConfigured(error_msg)
-
-SECRET_KEY = get_secret("SECRET_KEY")
-
-ALLOWED_HOSTS = [SECRET_KEY, '127.0.0.1',"*"]
-
+ALLOWED_HOSTS = ['127.0.0.1',"*"]
 
 # Application definition
 
