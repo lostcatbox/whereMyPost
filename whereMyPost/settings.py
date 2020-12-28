cfdf9dae702897d8b35b4cced368bc541ee0db8e
lostcatbox <lostcatbox@gmail.com>
2020-12-28 15:16:40 +0900

    Debug True

--- whereMyPost/settings.py
+++ whereMyPost/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=z#xqdq$0d%alf#w1-3#g@ikw0hm!x^*&yzy4$j#o0+t9$vhp&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1',"*"]
 
